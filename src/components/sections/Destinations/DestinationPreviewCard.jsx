b176dcd92678209a6c4aa2cbe493f67e30fa338b
Anold <anoldmwawasi@gmail.com>
2026-09-22 23:47:49 +0300

    Home refined

---
+++ src/components/sections/Destinations/DestinationPreviewCard.jsx
@@ -0,0 +1,39 @@
+// import { Link } from "react-router-dom";
+
+// function DestinationPreviewCard({ destination }) {
+//   return (
+//     <article className="destination-preview-card">
+//       <img
+//         className="destination-preview-card__image"
+//         src={destination.image}
+//         alt={destination.title}
+//       />
+
+//       <div className="destination-preview-card__overlay" />
+
+//       <div className="destination-preview-card__content">
+//         <span className="destination-preview-card__country">
+//           {destination.country}
+//         </span>
+
+//         <h3 className="destination-preview-card__title">
+//           {destination.title}
+//         </h3>
+
+//         <p className="destination-preview-card__description">
+//           {destination.description}
+//         </p>
+
+//         <Link
+//           to="/book"
+//           className="destination-preview-card__link"
+//         >
+//           Plan This Trip
+//           <span>→</span>
+//         </Link>
+//       </div>
+//     </article>
+//   );
+// }
+
+// export default DestinationPreviewCard;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,8 @@
 import Hero from "../../components/sections/Hero/Hero";
 import About from "../../components/sections/About/About";
 import Services from "../../components/sections/Services/Services";
-import FeaturedTours from "../../components/sections/FeaturedTours/FeaturedTours";
 import DestinationsPreview from "../../components/sections/Destinations/DestinationsPreview";
 import WhyChooseUs from "../../components/sections/WhyChooseUs/WhyChooseUs";
-import VisaServicesPreview from "../../components/sections/VisaServicesPreview/VisaServicesPreview";
-import BookingCTA from "../../components/sections/BookingCTA/BookingCTA";
 import PlanJourney from "../../components/sections/PlanJourney/PlanJourney";
 
 function Home() {
@@ -17,9 +14,6 @@ function Home() {
       <WhyChooseUs />
       <DestinationsPreview />
       <PlanJourney />
-      {/* <FeaturedTours />
-      <VisaServicesPreview />
-      <BookingCTA /> */}
     </>
   );
 }
